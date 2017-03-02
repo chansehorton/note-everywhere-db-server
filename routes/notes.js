2e2e93f3d93ce331b3296553a86c062d24197139
CHANSE HORTON <chansehorton@yahoo.com>
2017-03-02 14:25:34 -0700

    ready for production testing

--- routes/notes.js
+++ routes/notes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const boom = require('boom');
 
 router.get('/notes', (req, res, next) => {
+  console.log('get request made');
   const userId = req.query.userId;
   const url = decodeURIComponent(req.query.url);
 
@@ -16,10 +17,10 @@ router.get('/notes', (req, res, next) => {
       .first()
       .then((note) => {
         if (!note) {
-          //custom error
-          return next();
+          console.log('no note found');
+          return next(boom.create(400, 'Bad Request. note not found'));
         }
-
+        console.log('note found ', note);
         res.send(note);
       })
       .catch((err) => {
@@ -39,6 +40,7 @@ router.get('/notes', (req, res, next) => {
 });
 
 router.get('/notes/:id', (req, res, next) => {
+  console.log('get request made');
   if (!req.params.id && !req.params.id.parseInt()) {
     return next(boom.create(400, 'Bad request.'));
   }
@@ -48,10 +50,10 @@ router.get('/notes/:id', (req, res, next) => {
     .first()
     .then((note) => {
       if (!note) {
-        //custom error
-        return next();
+        console.log('get search, no note found');
+        return next(boom.create(400, 'Bad Request. Note does not exist.'));
       }
-
+      console.log('found note ', note);
       res.send(note);
     })
     .catch((err) => {
@@ -60,6 +62,7 @@ router.get('/notes/:id', (req, res, next) => {
 })
 
 router.post('/notes', (req, res, next) => {
+  console.log('post request made ', req.body);
   if (!req.body.url || !req.body.note) {
     return next(boom.create(400, 'Bad request. One or more required parameters are empty.'))
   }
@@ -69,11 +72,13 @@ router.post('/notes', (req, res, next) => {
     .andWhere('url', req.body.url)
     .then((note) => {
       if (note[0]) {
+        console.log('post search, no note found');
         return next(boom.create(400, 'Bad request. This note already exists.'))
       } else {
         knex('notes')
           .insert(req.body, '*')
           .then((postedNote) => {
+            console.log('new note posted ', postedNote);
             res.send(postedNote[0]);
           })
           .catch((err) => {
@@ -87,6 +92,7 @@ router.post('/notes', (req, res, next) => {
 });
 
 router.patch('/notes/:id', (req, res, next) => {
+  console.log('attempting to patch ', req.body);
   if (!req.body || !req.body.note) {
     return next(boom.create(400, 'Bad request. Missing required data.'))
   }
@@ -95,12 +101,14 @@ router.patch('/notes/:id', (req, res, next) => {
     .first()
     .then((note) => {
       if (!note) {
+        console.log('patch - no note found');
         return next(boom.create(400, 'Bad request. This note does not exist.'))
       } else {
         knex('notes')
           .update(req.body, '*')
           .where('id', req.params.id)
           .then((updatedNote) => {
+            console.log('note patched ', updatedNote);
             res.send(updatedNote[0]);
           })
           .catch((err) => {
